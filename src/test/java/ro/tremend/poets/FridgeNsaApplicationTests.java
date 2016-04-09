ed45392d7c9f793bac8b0a63d06debde4b1e506a
Andrei Vlad <olteanuav@gmail.com>
2016-04-09 11:26:07 +0300

    - initial commit

---
+++ src/test/java/ro/tremend/poets/FridgeNsaApplicationTests.java
@@ -0,0 +1,18 @@
+package ro.tremend.poets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FridgeNsaApplication.class)
+@WebAppConfiguration
+public class FridgeNsaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
